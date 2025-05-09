9e607283d881b8e91d7cd1d27d37485d550f4e95
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-05-10 00:27:17 +0300

    added local and remote datasources without any implementation

--- SportsProject/data/sources/remote/RemoteDataSource.swift
+++ SportsProject/data/sources/remote/RemoteDataSource.swift
@@ -1,8 +1,38 @@
-//
-//  RemoteDataSource.swift
-//  SportsProject
-//
-//  Created by Macos on 09/05/2025.
-//
-
 import Foundation
+
+
+protocol RemoteDataSource {
+    func getLeaguesBySport(
+        sport: Sports,
+        onSuccess: @escaping ([LeagueDTO]) -> Void,
+        onFailure: @escaping (String) -> Void
+    )
+    
+    func getLeagueEvents(
+        sport: Sports,
+        leagueId: Int,
+        from: Date,
+        to: Date,
+        onSuccess: @escaping ([EventDTO]) -> Void,
+        onFailure: @escaping (String) -> Void
+    )
+    
+    func getTeamsByLeagueId(
+        sport: Sports,
+        leagueId: Int,
+        onSuccess: @escaping ([TeamDTO]) -> Void,
+        onFailure: @escaping (String) -> Void
+    )
+    
+    func getTeamById(
+        sport: Sports,
+        teamId: Int,
+        onSuccess: @escaping ([TeamDTO]) -> Void,
+        onFailure: @escaping (String) -> Void
+    )
+    
+    
+    
+    
+    
+}
