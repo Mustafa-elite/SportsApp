0e355209d8dd682da35530f792fccbb346ba5f1b
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-05-11 17:59:04 +0300

    created presenter for leauges

---
+++ SportsProject/leagues/presenter/LeaguesPresenter.swift
@@ -0,0 +1,32 @@
+import Foundation
+
+class LeaguesPresetner {
+    private var view: LeaguesView!
+    private var repo: Repository!
+    
+    init(view: LeaguesView!, repo: Repository!) {
+        self.view = view
+        self.repo = repo
+    }
+    
+    
+    func loadLeaguesBySport(sport: Sports) {
+        print("presenter has started loading")
+        repo.getLeaguesBySport(sport: sport) { leagues in
+            let viewLeagues = leagues.filter { league in
+                league.leagueLogo != nil || league.leagueLogo?.count ?? 0 > 0
+            }.map { league in
+                return league.toView()
+            }
+            self.view.setLeagues(leagues: viewLeagues)
+            
+        } onFailure: { errorMsg in
+            self.view.setError(errorMsg: errorMsg)
+        }
+
+        
+    }
+    
+    
+    
+}
