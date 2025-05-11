7d4b81e20400e0284c423d4e4d32b52530f116c3
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-05-11 15:43:41 +0300

    created class associated with chooser ui

---
+++ SportsProject/chooser/view/ChooserOptionCollectionViewCell.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ChooserOptionCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var ivOption: UIImageView!
+    
+    @IBOutlet weak var lbOption: UILabel!
+    
+}
