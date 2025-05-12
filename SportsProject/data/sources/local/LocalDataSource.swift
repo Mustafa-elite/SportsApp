217e00d969bb777d1496895fcc3eb4ba1c2b5aa6
Abdelrahman Ahmed Hamdy <90092220+Boodyahmedhamdy@users.noreply.github.com>
2025-05-12 18:12:49 +0300

    updated methods inside the local data source

--- SportsProject/data/sources/local/LocalDataSource.swift
+++ SportsProject/data/sources/local/LocalDataSource.swift
@@ -8,5 +8,20 @@
 import Foundation
 
 protocol LocalDataSource {
-    //
+
+    func getAllLeagues() -> [LeagueEntity]
+    func insertLeague(league: LeagueView)
+    func deleteLeauge(league: LeagueView)
+    
+}
+
+extension LeagueEntity {
+    func toView() -> LeagueView {
+        let id = Int(self.leaugeId ?? "0") ?? 0
+        let name = self.leagueName ?? "NONE"
+        let url = self.leagueImageUrl ?? "NONE"
+        let sportsId = self.sportId ?? "NONE"
+        
+        return LeagueView(id: id, name: name, logoUrl: url, sportId: sportsId)
+    }
 }
